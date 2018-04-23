8b176b260ebf19f78eb31af813897d3fa81a301a
duongthienly <duongthienlee@gmail.com>
2018-04-23 12:42:23 +0300

    Fix carousel

--- personal_trainer/src/App.js
+++ personal_trainer/src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
                             <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
                         </ol>
                         <div className="carousel-inner">
                             <div className="carousel-item active">
